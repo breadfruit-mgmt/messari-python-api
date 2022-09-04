924d31d31530929b99ddbbafe32a877db03769df
breadfruit-mgmt <110607103+breadfruit-mgmt@users.noreply.github.com>
2022-09-04 15:43:39 -0400

    Added if-statement to continue slug loop, line 543

--- messari/messari/messari.py
+++ messari/messari/messari.py
@@ -540,6 +540,8 @@ class Messari(DataLoader):
         for asset in asset_slugs:
             url = base_url_template.substitute(asset_key=asset)
             response = self.get_response(url, params=payload, headers=self.api_dict)
+            if response = 1:
+                continue
             response_flat = convert_flatten(response['data'])
             response_data[asset] = response_flat
         if to_dataframe:
