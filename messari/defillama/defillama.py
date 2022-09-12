46c039803e13099437b17b61aa78979b7154971f
breadfruit-mgmt <110607103+breadfruit-mgmt@users.noreply.github.com>
2022-09-12 12:11:08 -0400

    Update defillama.py

--- messari/defillama/defillama.py
+++ messari/defillama/defillama.py
@@ -62,7 +62,7 @@ class DeFiLlama(DataLoader):
         for slug in slugs:
             endpoint_url = DL_GET_PROTOCOL_TVL_URL.substitute(slug=slug)
             protocol = self.get_response(endpoint_url)
-            display(protocol)
+            #display(protocol)
             display(pd.DataFrame(protocol))
 
             ###########################
